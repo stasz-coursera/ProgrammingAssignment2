b41c171cd669f4cf9b44091bbbc73ca82b98d56c
stasz-coursera <stasz-coursera@mailismagic.com>
2015-03-22 23:12:08 +1030

    Add additional comments to clarify steps

--- cachematrix.R
+++ cachematrix.R
@@ -32,17 +32,21 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  
   m <- x$getInverseMatrix()
+
+  ## Check if the matrix was filled from the cache
   if(!is.null(m)) {
     message("Getting cached data...")
     return (m)
   }
-  
+  ## Since return wasn't called, the result was not retrieved from the cache
   message("Calculating inverse matrix...")
   data <- x$get()
+  
+  ## Caclulate the Inverse Matrix (assuming is is square)
   m <- solve(data, ...)
   x$setInverseMatrix(m)
+  
+  ## Output the resut
   m
 }
